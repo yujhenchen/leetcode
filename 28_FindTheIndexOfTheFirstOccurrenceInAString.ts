ec9a812ba155f0d78d663504007a7c0f4afc284a
yujhenchen <yujhen.chen93@gmail.com>
2023-10-01 14:11:02 +0800

    remove not need codes

--- 28_FindTheIndexOfTheFirstOccurrenceInAString.ts
+++ 28_FindTheIndexOfTheFirstOccurrenceInAString.ts
@@ -2,7 +2,7 @@ function strStr(haystack: string, needle: string): number {
     // Version 1
     let index = -1;
     for (let i = 0; i < haystack.length; i += 1) {
-        if (i + needle.length > haystack.length) return -1;
+        // if (i + needle.length > haystack.length) return -1;  // this is not needed
         if (haystack.substring(i, i + needle.length) === needle) return i;
     }
     return index;
