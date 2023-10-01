e4565ef1ab576707c044ec731c55ff2431b1e628
yujhenchen <yujhen.chen93@gmail.com>
2023-10-01 12:50:36 +0800

    remove not valid version

--- 28_FindTheIndexOfTheFirstOccurrenceInAString.ts
+++ 28_FindTheIndexOfTheFirstOccurrenceInAString.ts
@@ -1,12 +1,12 @@
 function strStr(haystack: string, needle: string): number {
     // Version 1
-    // let index = -1;
-    // for (let i = 0; i < haystack.length; i += 1) {
-    //     if (i + needle.length > haystack.length) return -1;
-    //     if (haystack.substring(i, i + needle.length) === needle) return i;
-    // }
-    // return index;
+    let index = -1;
+    for (let i = 0; i < haystack.length; i += 1) {
+        if (i + needle.length > haystack.length) return -1;
+        if (haystack.substring(i, i + needle.length) === needle) return i;
+    }
+    return index;
 
-    // Version 2
-    return haystack.indexOf(needle);
+    // Version 2 (This version is not allowed)
+    // return haystack.indexOf(needle);
 };
