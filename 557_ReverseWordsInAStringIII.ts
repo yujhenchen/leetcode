9d1cfd1845a652d8c6e39528fe8cabb2e8b70f1d
yujhenchen <yujhen.chen93@gmail.com>
2023-10-01 10:00:31 +0800

    add more solutions

--- 557_ReverseWordsInAStringIII.ts
+++ 557_ReverseWordsInAStringIII.ts
@@ -1,14 +1,31 @@
 function reverseWords(s: string): string {
+    // Version 1:
+    // let word = '';
+    // let result = '';
+    // const sArr = [...s];
+    // for (let i = 0; i < sArr.length; i += 1) {
+    //     if (sArr[i] === ' ') {
+    //         result += `${word} `;
+    //         word = '';
+    //     }
+    //     else word = `${sArr[i]}${word}`;
+    // }
+    // result += word;
+    // return result;
+
+    // Version 2
     let word = '';
     let result = '';
-    const sArr = [...s];
-    for (let i = 0; i < sArr.length; i += 1) {
-        if (sArr[i] === ' ') {
+    for (let char of s) {
+        if (char === ' ') {
             result += `${word} `;
             word = '';
         }
-        else word = `${sArr[i]}${word}`;
+        else word = `${char}${word}`;
     }
     result += word;
     return result;
+
+    // Version 3 (this is slower than the Version 1 and Version 2)
+    // return s.split(' ').map(word => [...word].reverse().join('')).join(' ');
 };
