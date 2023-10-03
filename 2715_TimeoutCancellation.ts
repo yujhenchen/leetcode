521808606f280f230ca32b7fc35106cea508ef49
yujhenchen <yujhen.chen93@gmail.com>
2023-10-03 20:03:57 +0800

    add solution

---
+++ 2715_TimeoutCancellation.ts
@@ -0,0 +1,8 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type Fn = (...args: JSONValue[]) => void
+
+function cancellable(fn: Fn, args: JSONValue[], t: number): Function {
+
+    const id = setTimeout(() => fn(...args), t);
+    return () => clearTimeout(id);
+};
