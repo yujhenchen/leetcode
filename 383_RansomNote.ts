1342626d92c6fd8fe39c3ba3d80025b57ab1808e
yujhenchen <yujhen.chen93@gmail.com>
2023-10-04 22:08:40 +0800

    optimize version

--- 383_RansomNote.ts
+++ 383_RansomNote.ts
@@ -1,27 +1,41 @@
 function canConstruct(ransomNote: string, magazine: string): boolean {
-    // use a map to keep magazine words count
-    // loop ransomNote
-    // if find char in map, count -1 for the map
-    // else return false
+    // Version 1
+    // // use a map to keep magazine words count
+    // // loop ransomNote
+    // // if find char in map, count -1 for the map
+    // // else return false
 
-    // if(ransomNote.length === 0 || magazine.length === 0) return false;
+    // // if(ransomNote.length === 0 || magazine.length === 0) return false;
 
-    const magazineCountMap = new Map<string, number>();
+    // const magazineCountMap = new Map<string, number>();
+    // let char = '';
+    // for(let i =0; i< magazine.length; i+= 1){
+    //     char = magazine.charAt(i);
+    //     if(magazineCountMap.get(char)) magazineCountMap.set(char, magazineCountMap.get(char)+1);
+    //     else magazineCountMap.set(char, 1);
+    // }
+
+    // // console.log(magazineCountMap);
+
+    // for(let i =0; i< ransomNote.length; i+= 1){
+    //     char = ransomNote.charAt(i);
+    //     if(magazineCountMap.get(char))
+    //     {
+    //         if(magazineCountMap.get(char) < 1) return false;
+    //         else magazineCountMap.set(char, magazineCountMap.get(char)-1);
+    //     }
+    //     else return false;
+    // }
+    // return true;
+
+
+    // Version 2
     let char = '';
-    for (let i = 0; i < magazine.length; i += 1) {
-        char = magazine.charAt(i);
-        if (magazineCountMap.get(char)) magazineCountMap.set(char, magazineCountMap.get(char) + 1);
-        else magazineCountMap.set(char, 1);
-    }
-
-    // console.log(magazineCountMap);
-
+    let tempMagazine = magazine;
     for (let i = 0; i < ransomNote.length; i += 1) {
         char = ransomNote.charAt(i);
-        if (magazineCountMap.get(char)) {
-            if (magazineCountMap.get(char) < 1) return false;
-            else magazineCountMap.set(char, magazineCountMap.get(char) - 1);
-        }
+        // console.log(`char: ${char}, tempMagazine: ${tempMagazine}`);
+        if (tempMagazine.indexOf(char) > -1) tempMagazine = tempMagazine.replace(char, '');
         else return false;
     }
     return true;
